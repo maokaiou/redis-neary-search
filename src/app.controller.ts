92a85a00b44a8d870a87e252cc485a91ab990488
maokaiou <2827017101@qq.com>
2024-08-30 17:48:52 +0800

    feat:radis methods

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,68 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Inject,
+  Query,
+} from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { RedisService } from './redis/redis.service';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+  constructor(
+    private readonly appService: AppService,
+    @Inject(RedisService) private readonly redisService: RedisService,
+  ) {}
+  @Get('addPos')
+  async addPos(
+    @Query('name') posName: string,
+    @Query('longitude') longitude: number,
+    @Query('latitude') latitude: number,
+  ) {
+    if (!posName || !longitude || !latitude) {
+      throw new BadRequestException('位置信息不全');
+    }
+    try {
+      await this.redisService.geoAdd('positions', posName, [
+        longitude,
+        latitude,
+      ]);
+    } catch (error: any) {
+      throw new BadRequestException(error.message);
+    }
+    return {
+      message: '添加成功',
+      statusCode: 200,
+    };
+  }
+  @Get('getPos')
+  async getPos(@Query('name') pos: string) {
+    return await this.redisService.getPos('positions', pos);
+  }
+  @Get('getList')
+  async getList() {
+    return await this.redisService.getList('positions');
+  }
+  @Get('getNeary')
+  async getNeary(
+    @Query('longitude') longitude: string,
+    @Query('latitude') latitude: string,
+    @Query('radius') radius: number,
+  ) {
+    if (!longitude || !latitude || !radius) {
+      throw new BadRequestException('位置信息不全');
+    }
+    try {
+      return await this.redisService.getNeary(
+        'positions',
+        [longitude, latitude],
+        radius,
+        'km',
+      );
+    } catch (e: any) {
+      throw new BadRequestException(e.message);
+    }
+  }
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -11,4 +11,44 @@ export class RedisService {
       member: posName,
     });
   }
+  async getPos(key: string, posName: string) {
+    const res = await this.redisClient.geoPos(key, posName);
+    return {
+      name: posName,
+      longitude: res[0].longitude,
+      latitude: res[0].latitude,
+    };
+  }
+  async getList(key: string) {
+    // geo 信息底层使用 zset 存储的，所以查询所有的 key 使用 zrange
+    const positions = await this.redisClient.zRange(key, 0, -1);
+    const posList = [];
+    for (const pos of positions) {
+      const posInfo = await this.getPos(key, pos);
+      posList.push(posInfo);
+    }
+    return posList;
+  }
+  async getNeary(
+    key: string,
+    pos: [number | string, number | string],
+    radius: number,
+    unit: 'm' | 'km' | 'mi' | 'ft' = 'km',
+  ) {
+    const posList = [];
+    const prototypeArry = await this.redisClient.GEORADIUS(
+      key,
+      {
+        longitude: pos[0],
+        latitude: pos[1],
+      },
+      radius,
+      unit,
+    );
+    for (const item of prototypeArry) {
+      const posInfo = await this.getPos(key, item);
+      posList.push(posInfo);
+    }
+    return posList;
+  }
 }
