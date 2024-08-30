32c59b4341af519b8613051e9c6fc5d8c0efecc1
maokaiou <2827017101@qq.com>
2024-08-30 11:15:42 +0800

    feat:radis connect

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { RedisService } from './redis.service';
-
+import { createClient } from 'redis';
 @Module({
-  providers: [RedisService]
+  providers: [
+    RedisService,
+    {
+      provide: 'REDIS_CLIENT',
+      async useFactory() {
+        const client = await createClient()
+          .on('error', (err) => console.log('Redis Client Error', err))
+          .connect();
+        return client;
+      },
+    },
+  ],
+  exports: [RedisService],
 })
 export class RedisModule {}
--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
-
+import { Inject, Injectable } from '@nestjs/common';
+import { RedisClientType } from 'redis';
 @Injectable()
-export class RedisService {}
+export class RedisService {
+  @Inject('REDIS_CLIENT')
+  public readonly redisClient: RedisClientType;
+  async geoAdd(key: string, posName: string, posLoc: [number, number]) {
+    return await this.redisClient.geoAdd(key, {
+      longitude: posLoc[0],
+      latitude: posLoc[1],
+      member: posName,
+    });
+  }
+}
